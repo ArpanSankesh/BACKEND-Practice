eee658c79166afdeea536838ea384eaecbb287e0
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-26 15:49:01 +0530

    Small mini backend Project

--- MINI PROJECT SHREYANSH/index.js
+++ MINI PROJECT SHREYANSH/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
-app.get('/', function(req , res){
-    res.render("index");
-});
-
-app.get('/profile/:username', function(req, res){
-    res.send(`Welcome , ${req.params.username}`);
-});
-
-app.get('/profile/:username/:age', function(req, res){
-    res.send(`Welcome , ${req.params.username}, your Age is ${req.params.age}`);
-});
+app.get('/', function(req, res){
+    res.send('WELCOME');
+})
 
 app.listen(3000, function(){
-    console.log('it is running');
+
 });
