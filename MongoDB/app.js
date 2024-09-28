a21f8c3d07bc8bdf3a242d6c6cbac2e810501762
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-28 18:10:24 +0530

    MONGODB

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 
+const userModel = require('./usermodel');
+
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 app.get('/', function(req, res){
     res.send('it is working')
+});
+
+app.get('/create', async (req, res) => {
+    let createdUser = await userModel.create({
+        name: "Max", 
+        username: "XuiXui__", 
+        email: "max@gamil.com"
+    });
+    res.send(createdUser);
+});
+
+app.get('/update', async (req, res) => {
+    let updateUser = await userModel.findOneAndUpdate({name: "Max"}, {name: "Maxxxx"}, {new : true});
+    res.send(updateUser);
+});
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.send(users);
+});
+
+app.get('/delete', async (req, res) => {
+    let users = await userModel.findOneAndDelete({name: "Max"});
+    res.send(users);
 });
 
 app.listen(3000);
