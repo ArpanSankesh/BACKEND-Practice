a1076e65c78d56d51effd7a6a160e5ea1fb84d62
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-30 19:43:12 +0530

    MONGODB

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 
 app.use(express.json());
@@ -9,8 +10,29 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send('it is working')
+    res.render('index');
 });
+
+app.get('/read', async function(req, res){
+    let users = await userModel.find();
+    res.render('read', {users});
+});
+
+app.post('/create', async function(req, res){
+    let {name, email, image} = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+    res.redirect('/read'); 
+});
+
+app.get('/delete/:id', async function(req, res){
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect('/read');
+});
+
 
 
 app.listen(3000);
