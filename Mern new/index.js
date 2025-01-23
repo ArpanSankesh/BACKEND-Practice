a785c69c356f325c067c2c61a437562483df6a6a
ArpanSankesh <arpansankesh830@gmail.com>
2025-01-23 17:21:25 +0530

    update

--- Mern new/index.js
+++ Mern new/index.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const app = express(); 
+
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+
+app.set('view engine', 'ejs')
+
+
+app.use((req, res, next) => {
+    const a = 10; 
+    const b = 20;
+    console.log(a+b);
+
+    return next();
+})
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000);
